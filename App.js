31e7403e890d464ebd213d5a9211522e8de16d59
Geraldalex <alex1989chekan@gmail.com>
2021-12-09 12:22:51 +0200

    commit

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {Home, Restaurant, OrderDelivery} from './src/screens'
+import Tabs from './src/navigation/tabs';
+
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator
+    screenOptions={{
+      headerShown:false
+    }}
+    initialRouteName={"Tabs"}
+    >
+      <Stack.Screen name="Tabs" component={Tabs}/>
+      <Stack.Screen name="Restaurant" component={Restaurant}/>
+      <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/index.js
@@ -0,0 +1,16 @@
+import Home from "./Home"
+import Restaurant from "./Restaurant"
+import OrderDelivery from "./OrderDelivery"
+import Like from "./Like"
+import Person from "./Person"
+import Search from "./Search"
+
+export {
+    Home,
+    Restaurant,
+    OrderDelivery,
+    Like,
+    Person,
+    Search
+
+}
